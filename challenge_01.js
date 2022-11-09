7a65b2bbe6c16d3dd7dda17ed84de45a528d0ec3
MichaelErichMueller <Michael.Erich.Mueller@gmx.de>
2022-11-09 11:57:44 +0100

    Update

--- challenge_01.js
+++ challenge_01.js
@@ -9,3 +9,41 @@ Geben Sie die Summe der Zahlen in die Konsole aus:
 "Die Summe der Zahlen ist: summe"
 */
 
+
+//console.log(parseInt(prompt("Erste Zahl: ")) + parseInt(prompt("Zweite Zahle: ")));
+
+// Variante 1
+
+// vorteil gute Lesbarkeit aber viele variablen
+
+
+//Deklaration
+
+let ziffer1, ziffer2;
+let zahl1, zahl2;
+let summe;
+
+// EIngabe Wertzuweißung :: Ziffern
+
+ziffer1 = prompt("Zahl1:");
+ziffer2 = prompt("Zahl2:");
+console.log(typeof ziffer1);
+console.log(typeof ziffer2);
+
+// Typenkonvertierung Ziffern --> Zahl (String too Number)
+zahl1 = parseInt(ziffer1);
+zahl2 = parseInt(ziffer2);
+console.log(typeof zahl1);
+console.log(typeof zahl2);
+
+// Berechnung :: Summe / Adition
+summe = zahl1 + zahl2;
+
+// Ausgabe
+console.log("Die Summe der Zahlen ist:" + summe);
+
+
+// summe = zahl1 + zahl2;
+
+// console.log("Die Summe der Zahlen ist:" + summe);
+
